1a1a4d60df6067cbeacadb702e1a2c07a54831f2
chad green <chadgreen@chads-MacBook-Pro.local>
2019-01-22 16:21:54 -0700

    sds

--- app/controllers/dataController.js
+++ app/controllers/dataController.js
@@ -47,7 +47,7 @@ module.exports = {
             .catch(err => res.status(422).json(err));
     },
     create: function (req, res) {
-        console.log("create hit")
+        // console.log("create hit")
         db.Data
             .create(req.body)
             .then(dbModel => res.json(dbModel))
