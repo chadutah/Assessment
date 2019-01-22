d70d40260b6ded99352ab1b9441b3b7e4cf96cd5
chad green <chadgreen@chads-MacBook-Pro.local>
2019-01-22 14:58:20 -0700

    sds

--- app/server.js
+++ app/server.js
@@ -9,7 +9,7 @@ var routes = require("./routes");
 
 
 // listen on port 3001
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3001;
 
 // Define middleware here
 // commented out from JJ		
